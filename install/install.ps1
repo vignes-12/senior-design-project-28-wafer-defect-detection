10f0a5806136a82719998bf1a79fb78a581687bd
nasulliv <nasulliv@ncsu.edu>
2023-04-26 10:09:22 -0400

    install

---
+++ install/install.ps1
@@ -0,0 +1,12 @@
+﻿pip install numpy
+pip install kivy
+pip install kivy-deps.angle
+pip install kivy-deps.sdl2
+pip install kivy-garden
+pip install matplotlib
+pip install opencv-python
+pip install Pillow
+pip install pixelink
+pip install pixelinkWrapper
+pip install wafermap
+pip install wfmap
